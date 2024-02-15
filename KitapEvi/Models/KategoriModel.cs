f716a311709bf526499681a155971582dbf08acc
anil481 <ulvitok56@gmail.com>
2024-02-15 09:17:21 +0300

    Proje dosyası ekle.

---
+++ KitapEvi/Models/KategoriModel.cs
@@ -0,0 +1,10 @@
+﻿namespace KitapEvi.Models
+{
+    public class KategoriModel
+    {
+        public int KitapId {  get; set; }
+        public string KitapName { get; set; }
+        public string SayfaSayi { get; set; }
+        List<KategoriModel>kitapListesi= new List<KategoriModel> { };
+    }
+}
